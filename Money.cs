7e4ac8c9eafde933a95078f4e1dd97f8dc402afe
dwayray <95455478+dwayray@users.noreply.github.com>
2025-07-23 22:10:44 +0700

    push

---
+++ Money.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double rate = 23000;
+
+        Console.Write("Nhap sso tien: ");
+        string input = Console.ReadLine();
+        double usd;
+
+        if (double.TryParse(input, out usd))
+        {
+
+            double vnd = usd * rate;
+            Console.WriteLine($"{usd} USD = {vnd:N0} VND");
+        }
+        else
+        {
+            Console.WriteLine("Gia tri khong hop le!");
+        }
+
+        Console.ReadLine();
+    }
+}
