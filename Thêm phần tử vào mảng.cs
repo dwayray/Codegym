69174b267fdc70e73366a7eaf72a4cdae1a81227
dwayray <95455478+dwayray@users.noreply.github.com>
2025-07-24 23:46:16 +0700

    push

---
+++ Thêm phần tử vào mảng.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Mang ban dau: ");
+        PrintArray(array);
+
+        Console.Write("Nhap gia tri can chen: ");
+        int value = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nhap vi tri chen: ");
+        int position = Convert.ToInt32(Console.ReadLine());
+
+
+        if (position < 0 || position > array.Length)
+        {
+            Console.WriteLine("Vi tri khong hop le");
+            return;
+        }
+
+        int[] newArray = new int[array.Length + 1];
+
+        for (int i = 0; i < position; i++)
+        {
+            newArray[i] = array[i];
+        }
+
+        newArray[position] = value;
+
+        for (int i = position; i < array.Length; i++)
+        {
+            newArray[i + 1] = array[i];
+        }
+
+        Console.WriteLine("Mang sau khi chen: ");
+        PrintArray(newArray);
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
