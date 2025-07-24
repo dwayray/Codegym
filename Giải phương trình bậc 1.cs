08decc19519e5bb3ad6b13650d7f01fb57498ea2
dwayray <95455478+dwayray@users.noreply.github.com>
2025-07-24 15:09:16 +0700

    push

---
+++ Giải phương trình bậc 1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Nhap b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        // Giải phương trình
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Pt co vo so nghiem");
+            }
+            else
+            {
+                Console.WriteLine("Pt trinh vo nghiem");
+            }
+        }
+        else
+        {
+            double x = -b / a;
+            Console.WriteLine($"Nghiem cua pt la x = {x}");
+        }
+    }
+}
