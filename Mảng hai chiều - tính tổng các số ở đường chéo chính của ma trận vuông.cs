2574e077a4e5881d7bd1142c571e07702670b6b7
dwayray <95455478+dwayray@users.noreply.github.com>
2025-07-25 00:19:08 +0700

    push

---
+++ Mảng hai chiều - tính tổng các số ở đường chéo chính của ma trận vuông.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap kich thuoc: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double[,] matrix = new double[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"Nhap phan tu [{i},{j}]: ");
+                    matrix[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += matrix[i, i];
+            }
+
+
+            Console.WriteLine($"\nTong cac phan tu: {sum}");
+        }
+    }
+}
