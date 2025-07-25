0b4dc77d8462069fb2eaf03bd49a70415e8aa2aa
dwayray <95455478+dwayray@users.noreply.github.com>
2025-07-25 19:47:31 +0700

    push

---
+++ Viết phương thức đếm số lần xuất hiện của kí tự trong chuổi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string str = "trung tam lap trinh codegym";
+
+            Console.Write("Nhap ki tu can dem: ");
+            char c = Console.ReadKey().KeyChar; 
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Ky tu '{c}' xuat hien {count} lan trong chuoi: ");
+        }
+    }
+}
