7a9aa3cb57f5ded6218151ab20367ed68eab775b
dwayray <95455478+dwayray@users.noreply.github.com>
2025-07-25 19:37:46 +0700

    push

---
+++ Viết phương thức xoá phần tử khỏi mảng.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap n ");
+            int N = int.Parse(Console.ReadLine());
+
+            int[] array = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write($"Phan tu thu {i}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Nhap phan tu xoa (X): ");
+            int X = int.Parse(Console.ReadLine());
+            int index_del = -1;
+            for (int i = 0; i < N; i++)
+            {
+                if (array[i] == X)
+                {
+                    index_del = i;
+                    break;
+                }
+            }
+
+            if (index_del == -1)
+            {
+                Console.WriteLine("Phan tu khong ton tai");
+            }
+            else
+            {
+                for (int i = index_del; i < N - 1; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+
+                N--;
+                Console.WriteLine("Mang sau khi xoa:");
+                for (int i = 0; i < N; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
