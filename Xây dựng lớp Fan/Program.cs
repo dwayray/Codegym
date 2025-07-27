7fc75d2f9886a4f5648d69664eb6a3ba14733e02
dwayray <95455478+dwayray@users.noreply.github.com>
2025-07-27 23:26:00 +0700

    push

---
+++ Xây dựng lớp Fan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Fanprogram;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Fan fan1 = new Fan();
+        fan1.Speed = Fan.FAST;
+        fan1.Radius = 10;
+        fan1.Color = "yellow";
+        fan1.On = true;
+
+        Fan fan2 = new Fan();
+        fan2.Speed = Fan.MEDIUM;
+        fan2.Radius = 5;
+        fan2.Color = "blue";
+        fan2.On = false;
+
+        Console.WriteLine("Fan 1: " + fan1.ToString());
+        Console.WriteLine("Fan 2: " + fan2.ToString());
+    }
+}
