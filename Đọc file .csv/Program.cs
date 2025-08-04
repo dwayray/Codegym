bf0c26da50ca171442257d8582a724bcb180dc5d
dwayray <95455478+dwayray@users.noreply.github.com>
2025-08-04 21:52:47 +0700

    push

---
+++ Đọc file .csv/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "ten.csv";
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File khong ton tai");
+            return;
+        }
+
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            Console.WriteLine("Ds tat ca cac quoc gia: ");
+
+            foreach (string line in lines)
+            {
+                string cleanedLine = line.Replace("\"", "");
+                string[] parts = cleanedLine.Split(',');
+
+                if (parts.Length >= 6)
+                {
+                    string countryCode = parts[4];
+                    string countryName = parts[5];
+
+                    Console.WriteLine($"🌍 {countryCode} - {countryName}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Loi khi doc file " + ex.Message);
+        }
+    }
+}
